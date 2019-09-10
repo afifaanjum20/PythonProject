520e83e02c7195c2b54b454064f980d31ff9ba66
afifaanjum20 <52885517+afifaanjum20@users.noreply.github.com>
2019-09-10 16:42:03 +0530

    python assesments

---
+++ Assignment.py
@@ -0,0 +1,10 @@
+s="python for life"
+str=s.split(" ")
+print(str[0])
+print(str[1])
+print(str[2])
+print(s[2:10])
+print(s[8:13])
+
+print(str[0],str[1],str[2].upper())
+print(str[0].upper(),str[1],str[2].upper())
